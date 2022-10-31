f2eae84fcb5632d9d3b993bdafb71644778bd405
MakotoFujino <116146224+MakotoFujino@users.noreply.github.com>
2022-10-31 21:40:17 +0800

    Add files via upload

---
+++ face_similarity.py
@@ -0,0 +1,81 @@
+import face_recognition
+import cv2
+from pathlib import Path
+from PIL import Image
+
+key = cv2. waitKey(1)
+webcam = cv2.VideoCapture(0)
+while True:
+    try:
+        check, frame = webcam.read()
+        print(check) #prints true as long as the webcam is running
+        print(frame) #prints matrix values of each framecd 
+        cv2.imshow("Capturing", frame)
+        key = cv2.waitKey(1)
+        if key == ord('s'): 
+            cv2.imwrite(filename='saved_img.jpg', img=frame)
+            webcam.release()
+            img_new = cv2.imread('saved_img.jpg', cv2.IMREAD_GRAYSCALE)
+            img_new = cv2.imshow("Captured Image", img_new)
+            cv2.waitKey(1650)
+            cv2.destroyAllWindows()
+            print("Processing image...")
+            img_ = cv2.imread('saved_img.jpg', cv2.IMREAD_ANYCOLOR)
+            print("Converting RGB image to grayscale...")
+            gray = cv2.cvtColor(img_, cv2.COLOR_BGR2GRAY)
+            print("Converted RGB image to grayscale...")
+            print("Resizing image to 28x28 scale...")
+            img_ = cv2.resize(gray,(28,28))
+            print("Resized...")
+            img_resized = cv2.imwrite(filename='saved_img-final.jpg', img=img_)
+            print("Image saved!")
+        
+            break
+        elif key == ord('q'):
+            print("Turning off camera.")
+            webcam.release()
+            print("Camera off.")
+            print("Program ended.")
+            cv2.destroyAllWindows()
+            break
+        
+    except(KeyboardInterrupt):
+        print("Turning off camera.")
+        webcam.release()
+        print("Camera off.")
+        print("Program ended.")
+        cv2.destroyAllWindows()
+        break
+
+
+# Load the image of the person we want to find similar people for
+known_image = face_recognition.load_image_file("saved_img.jpg")
+
+# Encode the known image
+known_image_encoding = face_recognition.face_encodings(known_image)[0]
+
+# Variables to keep track of the most similar face match we've found
+best_face_distance = 1.0
+best_face_image = None
+
+# Loop over all the images we want to check for similar people
+for image_path in Path("people").glob("*.png"):
+    # Load an image to check
+    unknown_image = face_recognition.load_image_file(image_path)
+
+    # Get the location of faces and face encodings for the current image
+    face_encodings = face_recognition.face_encodings(unknown_image)
+
+    # Get the face distance between the known person and all the faces in this image
+    face_distance = face_recognition.face_distance(face_encodings, known_image_encoding)[0]
+
+    # If this face is more similar to our known image than we've seen so far, save it
+    if face_distance < best_face_distance:
+        # Save the new best face distance
+        best_face_distance = face_distance
+        # Extract a copy of the actual face image itself so we can display it
+        best_face_image = unknown_image
+
+# Display the face image that we found to be the best match!
+pil_image = Image.fromarray(best_face_image)
+pil_image.show()
