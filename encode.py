c27e6bbd561bbcf2cb176e37854f1d861b5c5b6c
celinewaiii28 <115997806+celinewaiii28@users.noreply.github.com>
2022-11-20 17:14:42 +0800

    Update encode.py

--- encode.py
+++ encode.py
@@ -5,33 +5,41 @@ from pathlib import Path
 from PIL import Image
 import numpy as np 
 
-
+# this is a list to store the array with 128 list (encoded images)
 dict = []
 
-for i in range(14): #loop the file in the folder
+for i in range(14): #loop the png file in the folder 
     fromfolder = face_recognition.load_image_file("kpoptestfolder/image00" + str() + ".png")
     toencode =face_recognition.face_encodings(fromfolder)
     dict.append(toencode)
+# print(dict)
 
-print(dict)
+imagee = face_recognition.load_image_file('saved_img-final.png')
+face = face_recognition.face_encodings(imagee)
 
+buffer=[]; result=[] 
+# buffer[] is to store all results 
+# result[] is to store final result that we want 
 
-buffer=[]; result=[] #to create a list to put in a list of list 
-# buffer[] is to store all result 
-# result[] is to store wanted result 
+# for i in range(14):
+#     convert = np.array(dict) # convert list of numpy array of list > numpy array of list 
+#     box = (convert.tolist()) # numpy array of list > list 
+
+# # print(box)
 
 for i in range(14):
-    compare = face_recognition.compare_faces(['saved_img.png'], dict[0] , tolerance=0.03)
-
-    buffer.append(compare)
+    # compare = face_recognition.compare_faces(['saved_img.png'], dict[0] , tolerance=0.03)
+    buffer.append(dict)
     convert = np.array(buffer[i])
     box = (convert.tolist())
-    result.append(box[0])
-
-print(len(result[0]))
-
-
+    # result.append(box[0])
     
+    result = face_recognition.compare_faces(face, box, tolerance=0.03)
+
+
+print(result)
+print(type(result))
+
 
 
 
