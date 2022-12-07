ea93c47475c7f164d6fbe2786d8001b46fdc6adf
celinewaiii28 <115997806+celinewaiii28@users.noreply.github.com>
2022-12-07 19:40:21 +0800

    new.py

---
+++ new.py
@@ -0,0 +1,44 @@
+import face_recognition
+from pathlib import Path
+from PIL import Image
+import numpy as np 
+import pickle 
+
+with open("dict", "rb") as fp: 
+    dict = pickle.load(fp)
+
+# print(dict)
+# print("\n")
+# print(type(dict))
+
+image = face_recognition.load_image_file('test.png')
+face = face_recognition.face_encodings(image)
+
+buffer = []
+for i in range(30):  
+    compare = face_recognition.compare_faces([dict[i]], face[0], tolerance=0.08)
+    buffer.append(compare)
+
+result=[] 
+
+for i in range(30):
+    convert = np.array(buffer[i])  
+    box = (convert.tolist()) 
+    result.append(box[0])
+
+list = [] # this list is to store the count int 
+
+for i in range(30): 
+   count = 0  #loop count here so the count = 0 everytime it loop
+   for x in result[i]:  # this for loop is to count the number of true in each array 
+      if x == True: 
+         count = count + 1
+        #  print("Type of count is ", type(count))
+   list.append(count)
+
+maxindex = list.index(max(list)) # get the position of the index in the list by maximum value 
+print("Position with maximum number of True in the list: ", maxindex)
+original = Image.open("test.png")
+original.show()
+final = Image.open("kpop/image0" + str(maxindex) + ".png") #open img of the index with maximun value 
+final.show() #show image
---
+++ pickle-en.py
@@ -0,0 +1,19 @@
+import face_recognition
+import pickle 
+
+dict = [] 
+for i in range(30): #loop the file in the folder
+    fromfolder = face_recognition.load_image_file("kpop/image0" + str(i) + ".png")
+    toencode = face_recognition.face_encodings(fromfolder)
+    dict.append(toencode)
+
+# print(dict)
+
+with open("dict", "wb") as fp:
+    pickle.dump(dict, fp)
+
+with open("dict", "rb") as fp: 
+    encoded = pickle.load(fp)
+
+# print(encoded)
+print(type(encoded))
