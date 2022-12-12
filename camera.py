dd29f6636def10a33b4e89e53b5033acc630b820
E1leenn <lim.eileen415@gmail.com>
2022-12-12 14:07:45 +0800

    eileen

--- camera.py
+++ camera.py
@@ -9,7 +9,7 @@ import cv2
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml') 
 
 key = cv2. waitKey(0)
-webcam = cv2.VideoCapture(0)
+webcam = cv2.VideoCapture(1)
 
 while True:
         check, frame = webcam.read()
